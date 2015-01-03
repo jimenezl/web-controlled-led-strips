9e497735dafc820dadb95ff16502c85b453ebe32
Lisandro Jimenez <jimenezl@mit.edu>
2015-01-03 12:05:58 -0500

    trying to fix php bug

--- server/receiving_file.php
+++ server/receiving_file.php
@@ -9,62 +9,65 @@
     $reset = $_POST['sentReset'];
     $party = $_POST['sentParty'];
 
-    $currentFileData = trim(file_get_contents('data.txt'));
+    $fileContents = file_get_contents('data.txt');
 
-    $splitLine = explode(" ", $currentFileData);
+    if ($fileContents !== FALSE){
+        $trimmedFileContents = trim($fileContents);
 
-    if (!empty($togglePower)){
-    	if ($splitLine[14] === "on"){
-    		$splitLine[14] = "off";
-    	}
-    	else {
-    		$splitLine[14] = "on";
-    	}
-    } 
+        $splitLine = explode(" ", $trimmedFileContents);
 
-    if (!empty($colors)){
-    	$explodedColors = explode(" ", $colors);
-		$splitLine[1] = $explodedColors[0];
-		$splitLine[2] = $explodedColors[1];
-		$splitLine[3] = $explodedColors[2];
+        if (!empty($togglePower)){
+        	if ($splitLine[14] === "on"){
+        		$splitLine[14] = "off";
+        	}
+        	else {
+        		$splitLine[14] = "on";
+        	}
+        } 
 
-		$splitLine[5] = 0;
+        if (!empty($colors)){
+        	$explodedColors = explode(" ", $colors);
+    		$splitLine[1] = $explodedColors[0];
+    		$splitLine[2] = $explodedColors[1];
+    		$splitLine[3] = $explodedColors[2];
+
+    		$splitLine[5] = 0;
+        }
+
+        if (!empty($setting)){
+    		$splitLine[5] = $setting;
+        }
+
+        if (!empty($brightness)){
+    		$splitLine[7] = $brightness;
+        }
+
+        if (!empty($speed)){
+    		$splitLine[9] = $speed;
+        }
+
+        if (!empty($strobe)){
+    		$splitLine[11] = $strobe;
+        }
+
+        if (!empty($strobeToggle)){
+        	if ($splitLine[12] === "on"){
+        		$splitLine[12] = "off";
+        	}
+        	else {
+        		$splitLine[12] = "on";
+        	}
+    		
+        }
+
+        file_put_contents('data.txt', implode(" ", $splitLine));
+
+        if (!empty($party)){
+            file_put_contents('data.txt', file_get_contents('partyData.txt'));
+        }
+
+        if (!empty($reset)){
+    		file_put_contents('data.txt', file_get_contents('defaultData.txt'));
+        }
     }
-
-    if (!empty($setting)){
-		$splitLine[5] = $setting;
-    }
-
-    if (!empty($brightness)){
-		$splitLine[7] = $brightness;
-    }
-
-    if (!empty($speed)){
-		$splitLine[9] = $speed;
-    }
-
-    if (!empty($strobe)){
-		$splitLine[11] = $strobe;
-    }
-
-    if (!empty($strobeToggle)){
-    	if ($splitLine[12] === "on"){
-    		$splitLine[12] = "off";
-    	}
-    	else {
-    		$splitLine[12] = "on";
-    	}
-		
-    }
-
-    file_put_contents('data.txt', implode(" ", $splitLine));
-
-    if (!empty($party)){
-        file_put_contents('data.txt', file_get_contents('partyData.txt'));
-    }
-
-    if (!empty($reset)){
-		file_put_contents('data.txt', file_get_contents('defaultData.txt'));
-    }
-    
     ?>
