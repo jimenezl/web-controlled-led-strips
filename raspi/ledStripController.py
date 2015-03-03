c4b616558255240924cab472c2e27659b438c8b7
Lisandro Jimenez <jimenezl@mit.edu>
2015-03-02 23:03:00 -0500

    changed fade scalings

--- raspi/ledStripController.py
+++ raspi/ledStripController.py
@@ -99,9 +99,9 @@ class ledStripController(object):
                     self.setBrightness(self.GREEN_PIN, float(self.reportedGreenLevel))
                     self.setBrightness(self.RED_PIN, float(self.reportedRedLevel))
                 else:
-                    if ((self.currentTime - self.fadeTime)<(float(100 - self.fadeSpeed + 1.0)/5)):
+                    if ((self.currentTime - self.fadeTime)<(float(101 - self.fadeSpeed)/35)):
                         pass
-                    elif ((self.currentTime - self.fadeTime)>(float(100 - self.fadeSpeed + 1.0)/5)):
+                    elif ((self.currentTime - self.fadeTime)>(float(101 - self.fadeSpeed)/35)):
                         self.fade(self.userSetting)
                         self.turnAllOn()
                         self.fadeTime = self.currentTime
